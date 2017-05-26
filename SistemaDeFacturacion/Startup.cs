b88e786f42cbb0dc9601f52bfc37c554dd962252
Jorge Fuentes <jorge.fuentes.14288@gmail.com>
2017-05-26 12:14:40 -0600

    Agregar archivos de proyecto.

---
+++ SistemaDeFacturacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaDeFacturacion.Startup))]
+namespace SistemaDeFacturacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
